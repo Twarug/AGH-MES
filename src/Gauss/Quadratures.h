71dcfd6a0bfa3169dbca258075d0e12ff94339aa
Twarug <patryk280502@gmail.com>
2024-11-07 17:04:23 +0100

    Fix Quadratures

--- src/Gauss/Quadratures.h
+++ src/Gauss/Quadratures.h
@@ -11,6 +11,19 @@ namespace mes {
         std::vector<f32> weights;
     };
 
-    extern std::vector<Quadrature> quadratureGL;
+    inline static std::vector<Quadrature> quadratureGL = {
+        {
+            { 0.0 },
+            { 2.0 }
+        },
+        {
+            { -0.5773502691896257, 0.5773502691896257 },
+            { 1.0, 1.0 }
+        },
+        {
+            { -0.7745966692414834, 0.0, 0.7745966692414834 },
+            { 0.5555555555555556, 0.8888888888888888, 0.5555555555555556 }
+        },
+    };
 
 }
